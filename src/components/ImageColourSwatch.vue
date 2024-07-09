8d277db624dd3a4e5af2fbf686b3c152d3df3d69
marsupialmouse <16873013+marsupialmouse@users.noreply.github.com>
2024-07-09 21:33:00 +1000

    Updated ImageColourSwatch to the composition API

--- src/components/ImageColourSwatch.vue
+++ src/components/ImageColourSwatch.vue
@@ -1,31 +1,27 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  colour: {
+    type: String,
+    required: true,
+  },
+  mousePosition: {
+    type: Object,
+    required: true,
+  },
+})
+
+const swatchStyle = computed(() => ({
+  'background-color': props.colour,
+  top: props.mousePosition.y - 40 + 'px',
+  left: props.mousePosition.x + 10 + 'px',
+}))
+</script>
+
 <template>
   <div class="imagecolourswatch" :style="swatchStyle">&nbsp;</div>
 </template>
-
-<script>
-export default {
-  name: 'ImageColourSwatch',
-  props: {
-    colour: {
-      type: String,
-      required: true,
-    },
-    mousePosition: {
-      type: Object,
-      required: true,
-    },
-  },
-  computed: {
-    swatchStyle() {
-      return {
-        'background-color': this.colour,
-        top: this.mousePosition.y - 40 + 'px',
-        left: this.mousePosition.x + 10 + 'px',
-      }
-    },
-  },
-}
-</script>
 
 <style scoped lang="less">
 @import '../variables.less';
