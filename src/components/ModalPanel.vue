06abd5df1920b644476fd640fbf50bebb8c6d713
marsupialmouse <16873013+marsupialmouse@users.noreply.github.com>
2025-08-19 08:36:14 +1000

    Switch the modal to use the dialog element

--- src/components/ModalPanel.vue
+++ src/components/ModalPanel.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, onUnmounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref, watch } from 'vue'
 
 const props = defineProps({
   show: Boolean,
@@ -10,69 +10,56 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['close'])
+const dialog = ref(null)
 
-const isMouseDownOnWrapper = ref(false)
-
-const close = () => emit('close')
+function close() {
+  if (dialog.value) {
+    dialog.value.close()
+  }
+  emit('close')
+}
 
 function keyUp(event) {
-  if (event.target.tagName.toLowerCase() !== 'input' && event.key === 'Escape') {
+  if (
+    event.target.tagName.toLowerCase() !== 'input' &&
+    event.target.tagName.toLowerCase() !== 'textarea' &&
+    event.key === 'Escape'
+  ) {
     close()
   }
 }
 
-function wrapperMouseDown() {
-  isMouseDownOnWrapper.value = true
-}
-function wrapperMouseLeave() {
-  isMouseDownOnWrapper.value = false
-}
-function wrapperMouseUp() {
-  if (isMouseDownOnWrapper.value) {
-    isMouseDownOnWrapper.value = false
+function mouseDown(event) {
+  if (event.target === event.currentTarget) {
     close()
   }
 }
+
+watch(
+  () => props.show,
+  newValue => {
+    if (newValue) dialog.value.showModal()
+  },
+  { flush: 'post' }
+)
 
 onMounted(() => window.addEventListener('keyup', keyUp, false))
 onUnmounted(() => window.removeEventListener('keyup', keyUp))
 </script>
 
 <template>
-  <Teleport to="body">
-    <Transition name="modal">
-      <div v-if="props.show" class="overlay modal-mask">
-        <div
-          ref="wrapper"
-          class="modal-wrapper"
-          @mousedown.self="wrapperMouseDown"
-          @mouseup.self="wrapperMouseUp"
-          @mouseleave="wrapperMouseLeave"
-        >
-          <div class="modal-container" :style="{ width: props.width }" @click.stop>
-            <button class="modal-close iconbutton fas fa-times" @click.stop.prevent="close"></button>
-            <slot></slot>
-          </div>
-        </div>
-      </div>
-    </Transition>
-  </Teleport>
+  <dialog ref="dialog" v-if="props.show" @mousedown="mouseDown">
+    <div class="modal-container" :style="{ width: props.width }" @click.stop>
+      <button class="modal-close iconbutton fas fa-times" @click.stop.prevent="close"></button>
+      <slot></slot>
+    </div>
+  </dialog>
 </template>
 
 <style scoped lang="less">
 @import '../variables.less';
 
 .modal {
-  &-mask {
-    display: table;
-    transition: opacity 0.3s ease;
-  }
-
-  &-wrapper {
-    display: table-cell;
-    vertical-align: middle;
-  }
-
   &-container {
     width: 30rem;
     margin: 0 auto;
@@ -114,6 +101,49 @@ onUnmounted(() => window.removeEventListener('keyup', keyUp))
   &-leave-active .modal-container {
     -webkit-transform: scale(1.1);
     transform: scale(1.1);
+  }
+}
+
+/* Open state of the dialog  */
+dialog:open {
+  opacity: 1;
+  transform: scaleY(1);
+  border: 0;
+  background-color: transparent;
+}
+
+/* Closed state of the dialog   */
+dialog {
+  opacity: 0;
+  transform: scaleY(0);
+  transition: all 0.3s allow-discrete;
+}
+
+/* Before open state  */
+/* Needs to be after the previous dialog:open rule to take effect,
+    as the specificity is the same */
+@starting-style {
+  dialog:open {
+    opacity: 0;
+    transform: scaleY(1.1);
+  }
+}
+
+/* Transition the :backdrop when the dialog modal is promoted to the top layer */
+dialog::backdrop {
+  background-color: rgba(0, 0, 0, 0);
+  transition: all 0.3s allow-discrete;
+}
+
+dialog:open::backdrop {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+/* This starting-style rule cannot be nested inside the above selector
+because the nesting selector cannot represent pseudo-elements. */
+@starting-style {
+  dialog:open::backdrop {
+    background-color: rgb(0 0 0 / 0%);
   }
 }
 </style>
